dade46856428fc4ddc6dfe6882528e86f5a9c144
ParkBoyeon <bo3893@gmail.com>
2021-12-28 16:58:40 +0900

    Initial commit

---
+++ src/test/java/hello/core/CoreApplicationTest.java
@@ -0,0 +1,12 @@
+package hello.core;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplicationTest {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplicationTest.class, args);
+    }
+}
