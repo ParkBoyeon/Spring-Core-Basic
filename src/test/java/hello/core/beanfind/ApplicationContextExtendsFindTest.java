93ab35b8e482cee21c6fca7d439e1339d3f4d1f8
ParkBoyeon <bo3893@gmail.com>
2021-12-30 16:02:54 +0900

    [스프링 컨테이너와 스프링 빈] 다양한 설정 형식 지원 -자바 코드, XML

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 class ApplicationContextExtendsFindTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
